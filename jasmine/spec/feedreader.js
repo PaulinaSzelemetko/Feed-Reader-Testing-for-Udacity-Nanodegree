01fa03faf1edbfdb85199c3f7e2e83aefe4257e7
Paulina Szelemetko <p.szelemetko@gmail.com>
2018-06-08 14:57:00 +0200

    small changes in feedreader and readme

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -14,13 +14,13 @@ $(function() {
         //This function loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.
         allFeeds.forEach(function(feed){
             it('has an url', function(){
-                expect(feed.url).toBeDefined();
-                expect(feed.length).not.toBe(0);
+                expect(feed.url.length).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
             });
             //This function ensures feed has a name defined and that the name is not empty
             it('has a name', function(){
-                expect(feed.name).toBeDefined();
-                expect(feed.length).not.toBe(0);
+                expect(feed.name.length).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
             });
         });
     });
@@ -42,16 +42,16 @@ $(function() {
 
     describe('Initial entries', function(){
 
-        let entries = document.getElementsByClassName('entry-link').length;
         //This function loads feed before the actual test is run. It uses Jasmine done function.
         beforeEach(function (done){
             loadFeed(0, function() {
+                entries= $('.feed .entry').length;
                 done();
             });
         });
         //This test ensures that when the loadFeed function is called and completes its work,ther is at least one .entry element
         it('has at least one entry element in the feed', function (done){
-            expect(entries.length).not.toBe(0);
+            expect(entries).not.toBe(0);
             done();
         }); 
     });
@@ -59,7 +59,7 @@ $(function() {
     describe('New Feed Selection', function(){
         let feedBefore;
         let feedAfter;
-        let entries = document.getElementsByClassName('entry-link')[0];
+     
 
         //This function loads the feed twice with different parameter and stores feeds content into variable after each load.
         beforeEach(function (done){
